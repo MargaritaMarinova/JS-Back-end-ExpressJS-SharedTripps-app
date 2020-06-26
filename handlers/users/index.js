9d883298901cf180dce5f5716aeae2a73c724e98
MargaritaMarinova <margarita.tomova@gmail.com>
2020-06-26 11:00:06 +0300

    feat/connect-with-database

--- handlers/users/index.js
+++ handlers/users/index.js
@@ -1,19 +1,45 @@
 //const User = require('./User');
 const jwt = require('../../utils/jwt');
 const {cookie} = require('../../config/config');
+const User = require('./User');
 
 module.exports = {
     get: {
-        login(req, res, next){
+        login(req, res, next) {
             res.render('users/login.hbs')
         },
 
-        register(req, res, next){
+        register(req, res, next) {
             res.render('users/register.hbs');
         }
 
     },
     post: {
+        login(req, res, next ) {
+            const {email, password} = req.body;
+
+            User.findOne({email})
+            .then((user)=> {
+                return user.passwordsMatch(password)
+            }).then((result)=> console.log(result));
+        },
+
+        register(req, res, next) {
+            const {
+                email,
+                password,
+                rePassword
+            } = req.body;
+
+            User.create({
+                    email,
+                    password
+                })
+                .then((createdUser) => {
+                    console.log(createdUser)
+                    res.redirect('/user/login')
+                })
+        }
 
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,7 @@ const handler = require('../handlers/users');
  router.get('/login', handler.get.login);
  router.get('/register', handler.get.register);
 
-module.exports = router;
+ router.post('/login', handler.post.login);
+ router.post('/register', handler.post.register);
+
+module.exports = router; 
